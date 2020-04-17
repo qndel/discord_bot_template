c822c7d69d99d908e7f46d8d342aed0809f58844
qndel <stefan551@o2.pl>
2020-04-17 19:14:22 +0200

    printing stack trace

--- helper.py
+++ helper.py
@@ -1,21 +1,19 @@
 import os
+import pickle
 import asyncio
-from discord.ext import commands
-from datetime import datetime, timedelta
+
+def save_obj(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb+') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 def isHost():
     return os.path.exists("./hostPC")
-#############################ERRORS AND EVENTS
+    
 async def setBotVariable(b):
     global bot
     bot = b
-
-async def handleErrors(ctx,error):
-    ignored = (commands.CommandNotFound, commands.UserInputError, commands.CheckFailure)
-    error = getattr(error, 'original', error)
-    if isinstance(error, ignored):
-        return
-    print(ctx.author.name)
-    print(ctx.message)
-    print(ctx.message.content)
-    print(error)
+################
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import helper
 import discord
 import asyncio
@@ -6,23 +7,34 @@ import importlib
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 
-bot = commands.Bot(command_prefix='!')
-token_host = "" #testing bot token
-token_remote = "" #bot token
+######CONFIG##################
 admins = ["Qndel#2237"]
 allowed_channels = [""]
-open('token_host', 'r') as file:
-    token_host = file.read()
-open('token_remote', 'r') as file:
-    token_remote = file.read()
+######END_CONFIG##############
+
+
+bot = commands.Bot(command_prefix='!')   
+ 
+async def updateStatus():
+    activity = discord.Activity(name='Servers: '+str(len(bot.guilds)), type=discord.ActivityType.watching)
+    await bot.change_presence(activity=activity)
     
 @bot.check
 async def filter_channels(ctx):
     return ctx.guild is None or ctx.channel.name in allowed_channels
-    
-#@bot.event
-#async def on_command_error(ctx, error):
-#    await helper.handleErrors(ctx,error)
+  
+@bot.event
+async def on_command_error(ctx, error):
+    ignored = (commands.CommandNotFound, commands.UserInputError, commands.CheckFailure)
+    error = getattr(error, 'original', error)
+    if isinstance(error, ignored):
+        return
+    print(ctx.author.name)
+    print(ctx.message)
+    print(ctx.message.content)
+    print(error)
+    print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+    traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
     
 bot.remove_command("help")
 @bot.command(name='help', hidden=True)
@@ -32,7 +44,7 @@ async def commandHelp(ctx):
         if not y.cog_name and not y.hidden:
             cmdList.append(y.name)
     await ctx.send('**Commands:**\n> '+("\n> "+bot.command_prefix).join(cmdList)+'\nWrite command without arguments to see more info.')  
-                
+                 
 @bot.command(name='reload', hidden=True)
 async def commandReload(ctx, *, arg=None):
     if str(ctx.message.author) not in admins:
@@ -44,15 +56,24 @@ async def commandReload(ctx, *, arg=None):
         await ctx.send("Data reloaded.", delete_after=10.0)
     else:
         await ctx.send("Data reloaded.")
+
+@bot.command(name='howtoadd')
+async def commandHowToAdd(ctx):
+    await ctx.send('To add the bot to a discord server, the server admin must visit\n<'+discord.utils.oauth_url(bot.user.id)+'>\nand choose the server.') 
+
+@bot.event
+async def on_guild_join(guild):
+    await updateStatus()
+    
+@bot.event
+async def on_guild_remove(guild):
+    await updateStatus()
     
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     await helper.setBotVariable(bot)
-    activity = discord.Activity(name='!help for commands', type=discord.ActivityType.watching)
-    await bot.change_presence(activity=activity)
-    
-if helper.isHost():
-    bot.run(token_host)
-else:
-    bot.run(token_remote) 
+    await updateStatus()
+
+with open(("token_remote","token_host")[helper.isHost()], 'r') as file:
+    bot.run(file.read())
